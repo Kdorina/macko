3ed69d42ddc1000988471b1c40412059e07db297
Kovács Dorina <kovacsdorina2@gmail.com>
2022-09-15 11:09:02 +0200

    Micimackó gyakorlo

---
+++ resources/views/templates/mez.blade.php
@@ -0,0 +1,10 @@
+@extends("master.master")
+
+@section("title", "Ebéd")
+@section("content")
+
+<h1>Micimackó ebédje</h1>
+<h3>Méz</h3>
+
+
+@endsection
---
+++ resources/views/templates/micimacko.blade.php
@@ -0,0 +1,11 @@
+@extends("master.master")
+
+@section("title", "Micimackó")
+@section("content")
+
+<h1>Mackó oldal</h1>
+<h3>Füles</h3>
+<h3>Csacsi</h3>
+<h3>Tigris</h3>
+
+@endsection
